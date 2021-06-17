2d57ed219f7d8307b743f2f08ac5982086f8241c
alis <asyed5839@gmail.com>
2021-06-17 14:21:29 +0100

    Added a header element and wrapped inside Fragment
    The header contains h1 and button elements and it is surrounded by React
    Fragment.

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,6 +1,12 @@
+import { Fragment } from "react";
 const Header = () => {
     return (
-
+        <Fragment>
+            <header>
+                <h1>React Meals</h1>
+                <button>Cart</button>
+            </header>
+        </Fragment>
     );
 };
 
