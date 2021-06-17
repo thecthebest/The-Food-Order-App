3e2f5263f10f386848fa0efb1616cdfcbef8a763
alis <asyed5839@gmail.com>
2021-06-17 14:38:28 +0100

    Inserted styles from header stylesheet

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import { Fragment } from "react";
 import mealsImage from '../../assets/meals.jpg';
+import classes from 'Header.module.css';
 const Header = () => {
     return (
         <Fragment>
-            <header>
+            <header className={classes.header}>
                 <h1>React Meals</h1>
                 <button>Cart</button>
             </header>
-            <div>
+            <div className={classes["main-image"]}>
                 <img src={mealsImage} alt="A table full of food!" />
             </div>
         </Fragment>
