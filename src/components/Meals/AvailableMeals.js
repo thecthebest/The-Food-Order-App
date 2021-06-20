d35ffa792b5d930c8e9245d94d143ad28a6a29ab
alis <asyed5839@gmail.com>
2021-06-20 13:46:58 +0100

    Fixed accessibility issues
    Clicking on ANY label will always select the same, first input element.
    Screenreaders won't be able to connect labels + inputs correctly (since
    all labels point at the same input).

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -31,6 +31,7 @@ const AvailableMeals = (props) => {
     const mealsList = DUMMY_MEALS.map((meal) => {
         return <li>
           {<MealItem 
+          id={meal.id}
           key={meal.id}
           name={meal.name} 
           price={meal.price} 
