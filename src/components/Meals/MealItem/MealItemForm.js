0dd30137e9b618cea88b2aa4280b8d93aac22b17
alis <asyed5839@gmail.com>
2021-06-23 09:42:12 +0100

    useRef to move the data from child component
    The data is passed from the custom Input component to MealItemForm via
    ref. Since the child component is not an html tag React.forward has been
    added inside the Input component to facilitate the transfer of the user input.

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,13 +1,16 @@
 import classes from './MealItemForm.module.css';
 import Input from '../../UI/Input';
+import { useRef } from 'react';
 
 const MealItemForm = (props) => {
+    const amountInputRef = useRef();
     const submitHandler = (event) => {
         event.preventDefault();
+        const enteredAmount = amountInputRef.current.value;
     };
     return (
         <form className={classes.form} onSubmit={submitHandler}>
-            <Input label="Amount" input={{
+            <Input ref={amountInputRef} label="Amount" input={{
                 id: "amount_" + props.id,
                 type: "number",
                 min: "1",
