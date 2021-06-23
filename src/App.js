3778ae98fbce15ee902ccfaef25bfbc7152946c7
alis <asyed5839@gmail.com>
2021-06-23 07:54:58 +0100

    inserted Ability to open & close cart
    Functionality to open and close the cart by clicking on the cart button.
    It uses "useState" to store the state and render the cart components.
    Initially the state is set to false and it can be changed to true when
    the cart button is clicked, this causes the cart to be displayed and
    then when the "close" button on the cart is clicked then the state is set
    to "false" resulting the cart to be removed from the screen.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
