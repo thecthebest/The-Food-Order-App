22a3efeaed9f8f911259ed7519b5aeafa0088236
alis <asyed5839@gmail.com>
2021-06-23 09:59:23 +0100

    Checking the user input and dispaly a message
    IF statement checks the user input and dispaly message under the add
    button if invalid input
    recevied.

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,18 @@
 import classes from './MealItemForm.module.css';
 import Input from '../../UI/Input';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 const MealItemForm = (props) => {
     const amountInputRef = useRef();
+    const [amountIsValid, setAmountIsValid]= useState(true);
     const submitHandler = (event) => {
-        event.preventDefault();
+        event.preventDefault(false);
         const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+        if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+            setAmountIsValid();
+            return;
+        } 
     };
     return (
         <form className={classes.form} onSubmit={submitHandler}>
@@ -19,6 +25,7 @@ const MealItemForm = (props) => {
                 defaultValue: '1'
             }}/>
             <button>+ Add</button>
+            {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
         </form>
     );
 };
