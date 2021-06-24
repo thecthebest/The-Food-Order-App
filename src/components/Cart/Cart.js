edd738c82f5d14de75b4d81d1e17979c09441645
alis <asyed5839@gmail.com>
2021-06-24 12:02:31 +0100

    Functionality to display items and price in the cart
    React context has been added to get the the number of items and of the
    meals. This information is then extracted and displayed in the Cart when
    it is opened.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,16 @@
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
+import CartContext from '../../store/cart-context';
+import { useContext } from 'react';
 
 const Cart = (props) => {
+    const cartCtx = useContext(CartContext);
+    const totalAmount = `£${cartCtx.totalAmount.toFixed(2)}`;
+    
     const cartItems =
         <ul className={classes['cart-items']}>
             {
-                [{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => {
+                cartCtx.items.map((item) => {
                     return <li>{item.name}</li>;
                 })
             }
@@ -15,7 +20,7 @@ const Cart = (props) => {
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
-                <span>35.62</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={classes.actions}>
                 <button onClick={props.onClose}  className={classes['button--alt']}>Close</button>
