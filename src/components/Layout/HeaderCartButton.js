9343c3217f5fd341968609a589a5208ec4fb0c4e
alis <asyed5839@gmail.com>
2021-06-26 13:05:15 +0100

    Animating the cart when adding items
    For making the cart to animate "useState" and "useEffect" have been
    utilized. The const called "btnIsHighLighted" store either "true" or
    "false", this then used switch different style classes. When the state
    is "true" "classes.bump" is assigned otherwise empty string. In addition
    to that there is "useEffect" to change the class back. The "useEffect"
    incorporates a timer which is resposnible for changing the class back
    after 300ms. Finally there is a funtion which is used to reset the timer.

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,30 @@
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
-
         const cartCtx = useContext(CartContext);
-        const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+        const { items } = cartCtx;
+        const numberOfCartItems = items.reduce((curNumber, item) => {
             return curNumber + item.amount;
         }, 0);
-
+        const [btnIsHighLighted, setBtnIsHighLighted] = useState(false);
+        const btnClasses = `${classes.button} ${btnIsHighLighted ? classes.bump : ''}`;
+        useEffect(()=>{
+            if (items.length === 0) {
+                return;
+            }
+            setBtnIsHighLighted(true);
+            const timer = setTimeout(()=>{
+                setBtnIsHighLighted(false);
+            }, 300);
+            return () => {
+                clearTimeout(timer);
+            }
+        }, [items]);
     return (
-        <button onClick={props.onClick } className={classes.button}>
+        <button onClick={props.onClick } className={btnClasses}>
             <span className={classes.icon}>
                 <CartIcon />
             </span>
